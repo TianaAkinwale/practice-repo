2738ea4ce163c1bf17507607e4927852fd394e5f
TianaAkinwale <tianorway@gmail.com>
2022-07-22 16:49:58 +0100

    exercise level 2

--- JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-2.js
+++ JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-2.js
@@ -8,8 +8,20 @@ console.log(typeof 10 == parseInt('9.8'))
 
 let check = 'python jargon'
 
-console.log(check.match('on'))
+console.log(check.search('on'))
 
 let check2 = "I hope this course is not full of jargon"
 
-console.log(check2.match('jargon'))
+console.log(check2.search('jargon'))
+
+let num = Math.floor(Math.random () * 101) // creates random number between 0 and 100
+
+console.log(num)
+
+let num2 = Math.floor(Math.random () * 256) // creates random number between 0 and 255
+
+console.log(num2)
+
+let js = 'JavaScript'
+
+console.log(Math.random(js) * 11)
