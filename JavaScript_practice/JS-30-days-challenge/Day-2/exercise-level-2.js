780ce16ba3094f72d3d24d69464162f9bb66780c
TianaAkinwale <tianorway@gmail.com>
2022-07-22 16:34:05 +0100

    exercise level 2

---
+++ JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-2.js
@@ -0,0 +1,15 @@
+console.log(`The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' 
+by John Holmes teaches us to help one another.`)
+
+console.log(`\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the 
+same -- with charity you give love, so don't just give money but reach out your hand instead.\"`)
+
+console.log(typeof 10 == parseInt('9.8'))
+
+let check = 'python jargon'
+
+console.log(check.match('on'))
+
+let check2 = "I hope this course is not full of jargon"
+
+console.log(check2.match('jargon'))
