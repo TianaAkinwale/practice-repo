b4a8aa8d351381eb20f30a07794748d44306b42d
TianaAkinwale <tianorway@gmail.com>
2022-07-23 02:15:57 +0100

    new

--- JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-2.js
+++ JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-2.js
@@ -4,7 +4,7 @@ by John Holmes teaches us to help one another.`)
 console.log(`\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the 
 same -- with charity you give love, so don't just give money but reach out your hand instead.\"`)
 
-console.log(typeof 10 == parseInt('9.8'))
+console.log(typeof 10 === parseInt('10'))
 
 let check = 'python jargon'
 
