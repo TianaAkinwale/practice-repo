ae6e4b43a6242392f85e39d408a0acfdbc6528ad
TianaAkinwale <tianorway@gmail.com>
2022-07-23 19:49:17 +0100

    exercise level 2

--- JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-2.js
+++ JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-2.js
@@ -4,24 +4,27 @@ by John Holmes teaches us to help one another.`)
 console.log(`\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the 
 same -- with charity you give love, so don't just give money but reach out your hand instead.\"`)
 
-console.log(typeof 10 === parseInt('10'))
-
 let check = 'python jargon'
-
 console.log(check.search('on'))
 
 let check2 = "I hope this course is not full of jargon"
-
 console.log(check2.search('jargon'))
 
 let num = Math.floor(Math.random () * 101) // creates random number between 0 and 100
-
 console.log(num)
 
 let num2 = Math.floor(Math.random () * 256) // creates random number between 0 and 255
-
 console.log(num2)
 
-let js = 'JavaScript'
+//var number = Math.floor(Math.random() * (max - min) + min);
+let num3 = Math.floor(Math.random () * (100 - 50) + 50)
+console.log(num3)
 
-console.log(Math.random(js) * 11)
+console.log(`1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125`)
+
+let word = "You cannot end a sentence with because because because is a conjunction"
+console.log(word.substr(31, 23))
