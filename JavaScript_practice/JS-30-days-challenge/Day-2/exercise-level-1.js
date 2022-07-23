708882214018a3c5a941643e5951b8c25f1a4a60
TianaAkinwale <tianorway@gmail.com>
2022-07-23 19:48:45 +0100

    exercise level 1

--- JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-1.js
+++ JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-1.js
@@ -8,23 +8,21 @@ console.log(challenge.split())
 console.log(challenge.split(' '))
 
 let names = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
-
 console.log(names.split(','))
 console.log(challenge.replace('JavaScript', 'Python'))
 console.log(names.charAt(15))
 console.log(names.charCodeAt('J'))
 
 let example = "You cannot end a sentence with because because because is a conjunction"
-
 console.log(example.indexOf('because')) 
 console.log(example.lastIndexOf('because'))
 console.log(example.search('because'))
+
 console.log(challenge.trim(' '))
 console.log(challenge.startsWith("30 Days Of JavaScript"))
 console.log(challenge.endsWith("30 Days Of JavaScript"))
 console.log(challenge.match('a'))
 
 let word = '30'
-
 console.log(word.concat("Days ", "Of ", "JavaScript "))
 console.log(challenge.repeat(2))
