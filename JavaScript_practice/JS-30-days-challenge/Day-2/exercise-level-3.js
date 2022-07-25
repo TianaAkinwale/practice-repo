0f030e5c53853c0f3d978808d45a96dd819b1e3a
TianaAkinwale <tianorway@gmail.com>
2022-07-25 16:26:21 +0100

    updated

--- JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-3.js
+++ JavaScript_practice/JS-30-days-challenge/Day-2/exercise-level-3.js
@@ -1,10 +1,14 @@
+/*1. 'Love is the best thing in this world.
+ Some found their love and some are still looking for their love.' Count the number of word love in this sentence. */
 let help = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
 console.log(help.match('love'))
 
+/*2. Use match() to count the number of all because in the following sentence:'You 
+cannot end a sentence with because because because is a conjunction' */
 let string = "You cannot end a sentence with because because because is a conjunction"
 console.log(string.match('because'))
+
+/*4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000
+ euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.' */
 let word = "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month."
 console.log(word.match(/\d+/g))
-
-const now = new Date()
-console.log(now.getHours()) // Sat Jan 04 2020 00:56:41 GMT+0200 (Eastern European Standard Time)
