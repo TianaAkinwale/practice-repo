63a666eaf0898e484a62eb50c7fd90319cbb432e
TianaAkinwale <tianorway@gmail.com>
2022-07-25 16:05:18 +0100

    commented

--- JavaScript_practice/JS-30-days-challenge/Day-1/age.js
+++ JavaScript_practice/JS-30-days-challenge/Day-1/age.js
@@ -1,3 +1,8 @@
+/*10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+I am 25 years old.
+You are 30 years old. */
+
 let myAge = 100
 let yourAge = 59
 
