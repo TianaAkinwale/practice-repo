eef64ad476871eb624a18b54d36d045625ee9f53
TianaAkinwale <tianorway@gmail.com>
2022-07-26 22:54:28 +0100

    exercise level 3

---
+++ JavaScript_practice/JS-30-days-challenge/Day-4/exercise-level-3.js
@@ -0,0 +1,16 @@
+/*1. Write a program which tells the number of days in a month.
+  Enter a month: January
+  January has 31 days.
+
+  Enter a month: JANUARY
+  January has 31 day
+
+  Enter a month: February
+  February has 28 days.
+
+  Enter a month: FEbruary
+  February has 28 days.*/
+
+
+
+/*2. Write a program which tells the number of days in a month, now consider leap year.*/
