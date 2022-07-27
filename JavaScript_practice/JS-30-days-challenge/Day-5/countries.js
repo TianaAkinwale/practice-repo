2cb3d5b5cf3d5ecaa98e300d52c6c4c30d4191ac
TianaAkinwale <tianorway@gmail.com>
2022-07-27 03:28:49 +0100

    countries

---
+++ JavaScript_practice/JS-30-days-challenge/Day-5/countries.js
@@ -0,0 +1,15 @@
+var countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+
+export{countries}
