0e9ec929b11e0864454674d7ba6f58c489cd4c57
TianaAkinwale <tianorway@gmail.com>
2022-07-31 01:28:39 +0100

    new

---
+++ JavaScript_practice/JS-30-days-challenge/Day-7/exercise-leve-1.js
@@ -0,0 +1,94 @@
+/*1. Declare a function _fullName_ and it print out your full name.*/
+function _fullName_ () {
+    console.log('Tiana Akinwale')
+}
+console.log(_fullName_());
+
+/*2. Declare a function _fullName_ and now it takes firstName, lastName as a
+ parameter and it returns your full - name.*/
+function _fullName_ (firstName, lastName) {
+    let name = `Your full name name is ${firstName} ${lastName}`;
+    return name;
+}
+console.log(_fullName_('Tiana', 'Akinwale'));
+/*3. Declare a function _addNumbers_ and it takes two two parameters and it returns sum.*/
+function _addNumbers_ (a, b) {
+    return a + b;
+}
+console.log(_addNumbers_(5, 20))
+
+/*4. An area of a rectangle is calculated as follows: _area = length x width_. 
+Write a function which calculates _areaOfRectangle_.*/
+function _area0fRectangle_ (length, width) {
+    let _area = length * width;
+    return _area;
+}
+console.log(_area0fRectangle_(2, 15));
+
+/*5. A perimeter of a rectangle is calculated as follows: 
+_perimeter= 2x(length + width)_. Write a function which 
+calculates _perimeterOfRectangle_.*/
+function _perimeterOfRectangle_ (length, width) {
+    let _perimeter = 2 * (length + width);
+    return _perimeter;
+}
+console.log(_perimeterOfRectangle_(4, 5))
+/*6. A volume of a rectangular prism is calculated as follows:
+ _volume = length x width x height_. Write a function which
+  calculates _volumeOfRectPrism_.*/
+  function _volume0fRectPrism (length, width, height) {
+    let _volume = (length * width * height);
+    return _volume;
+}
+console.log(_volume0fRectPrism(3, 4, 5))
+
+/*7. Area of a circle is calculated as follows: _area = π x r x r_. 
+Write a function which calculates _areaOfCircle_*/
+function _areaOfCircle_ (r) {
+    const PI = 3.142
+    let _perimeter =  PI * (r * r);
+    return _perimeter;
+}
+console.log(_areaOfCircle_(4))
+
+/*8. Circumference of a circle is calculated as follows:
+ _circumference = 2πr_. Write a function which calculates _circumOfCircle_*/
+ function _circumOfCircle_ (r) {
+    const PI = 3.142
+    let _circumference =  (2 * PI * r);
+    return _circumference;
+}
+console.log(_areaOfCircle_(4))
+
+/*9. Density of a substance is calculated as follows:_density= mass/volume_. 
+Write a function which calculates _density_.*/
+/*10. Speed is calculated by dividing the total distance covered by a moving 
+object divided by the total amount of time taken. Write a function which 
+calculates a speed of a moving object, _speed_.*/
+/*11. Weight of a substance is calculated as follows: _weight = mass x gravity_.
+ Write a function which calculates _weight_.*/
+/*12. Temperature in oC can be converted to oF using this formula: 
+_oF = (oC x 9/5) + 32_. Write a function which 
+convert oC to oF _convertCelsiusToFahrenheit_.*/
+/*13. Body mass index(BMI) is calculated as follows: _bmi = weight in Kg / (height x height) 
+in m2_. Write a function which calculates _bmi_. BMI is used to broadly define different
+ weight groups in adults 20 years old or older.Check if a person is _underweight, normal, 
+ overweight_ or _obese_ based the information given below.
+
+    - The same groups apply to both men and women.
+    - _Underweight_: BMI is less than 18.5
+    - _Normal weight_: BMI is 18.5 to 24.9
+    - _Overweight_: BMI is 25 to 29.9
+    - _Obese_: BMI is 30 or more*/
+
+/*14. Write a function called _checkSeason_, it takes a month parameter and returns the
+ season:Autumn, Winter, Spring or Summer.*/
+/*15. Math.max returns its largest argument. Write a function findMax that takes three
+ arguments and returns their maximum with out using Math.max method.
+
+    ```js
+    console.log(findMax(0, 10, 5))
+    10
+    console.log(findMax(0, -10, -2))
+    0
+    ```*/
---
+++ JavaScript_practice/JS-30-days-challenge/Day-7/exercise-level-2.js
@@ -0,0 +1,95 @@
+/*1. Linear equation is calculated as follows: _ax + by + c = 0_. Write a function
+ which calculates value of a linear equation, _solveLinEquation_.*/
+/*1. Quadratic equation is calculated as follows: _ax2 + bx + c = 0_. Write a 
+function which calculates value or values of a quadratic equation, _solveQuadEquation_.
+
+    ```js
+    console.log(solveQuadratic()) // {0}
+    console.log(solveQuadratic(1, 4, 4)) // {-2}
+    console.log(solveQuadratic(1, -1, -2)) // {2, -1}
+    console.log(solveQuadratic(1, 7, 12)) // {-3, -4}
+    console.log(solveQuadratic(1, 0, -4)) //{2, -2}
+    console.log(solveQuadratic(1, -1, 0)) //{1, 0}
+    ```*/
+
+/*1. Declare a function name _printArray_. It takes array as a parameter and 
+it prints out each value of the array.*/
+/*function _printArray_ (array) {
+ for (const eachValue of array) {
+  console.log(eachValue)
+}
+    return eachValue
+}
+const anc = [54, 34, 5]
+console.log(eachValue(anc))*/
+/*1. Write a function name _showDateTime_ which shows time in this format:
+ 08/01/2020 04:08 using the Date object.
+
+    ```sh
+    showDateTime()
+    08/01/2020 04:08
+    ```*/
+
+/*1. Declar00e a function name _swapValues_. This function swaps value of x to y.
+
+    ```js
+    swapValues(3, 4) // x => 4, y=>3
+    swapValues(4, 5) // x = 5, y = 4
+    ```*/
+
+/*1. Declare a function name _reverseArray_. It takes array as a parameter and 
+it returns the reverse of the array (don't use method).
+
+    ```js
+    console.log(reverseArray([1, 2, 3, 4, 5]))
+    //[5, 4, 3, 2, 1]
+    console.log(reverseArray(['A', 'B', 'C']))
+    //['C', 'B', 'A']
+    ```*/
+
+/*1. Declare a function name _capitalizeArray_. It takes array as a parameter
+ and it returns the - capitalizedarray.*/
+/*1. Declare a function name _addItem_. It takes an item parameter and it 
+returns an array after adding the item*/
+/*1. Declare a function name _removeItem_. It takes an index parameter and 
+it returns an array after removing an item*/
+/*1. Declare a function name _sumOfNumbers_. It takes a number parameter and 
+it adds all the numbers in that range.*/
+/*1. Declare a function name _sumOfOdds_. It takes a number parameter and
+ it adds all the odd numbers in that - range.*/
+/*1. Declare a function name _sumOfEven_. It takes a number parameter and 
+it adds all the even numbers in that - range.*/
+/*1. Declare a function name evensAndOdds . It takes a positive integer as
+ parameter and it counts number of evens and odds in the number.
+
+    ```sh
+    evensAndOdds(100);
+    The number of odds are 50.
+    The number of evens are 51.
+    ```*/
+
+/*1. Write a function which takes any number of arguments and return the 
+sum of the arguments
+
+    ```js
+    sum(1, 2, 3) // -> 6
+    sum(1, 2, 3, 4) // -> 10
+    ```*/
+
+/*1. Writ a function which generates a _randomUserIp_.*/
+/*1. Write a function which generates a _randomMacAddress_*/
+/*1. Declare a function name _randomHexaNumberGenerator_. When this function is called it
+ generates a random hexadecimal number. The function return the hexadecimal number.
+
+    ```sh
+    console.log(randomHexaNumberGenerator());
+    '#ee33df'
+    ```*/
+
+/*1. Declare a function name _userIdGenerator_. When this function is called it 
+generates seven character id. The function return the id.
+
+    ```sh
+    console.log(userIdGenerator());
+    41XTDbE
+    ```*/
