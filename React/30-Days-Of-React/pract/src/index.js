df0865bfc0379adc2e90ecad03728cf19174657a
TianaAkinwale <tianorway@gmail.com>
2022-10-15 15:21:33 +0100

    new

--- React/30-Days-Of-React/pract/src/index.js
+++ React/30-Days-Of-React/pract/src/index.js
@@ -1,17 +1,74 @@
+// importing the react and react-dom package and style file
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Tiana from './images/slack-dp.jpeg'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const user = (
+    <div>
+        <img src={Tiana} alt='Tiana Akinwale'/>
+    </div>
+)
+
+const subscribe = 'SUBSCRIBE'
+const signUp = 'Sign up with your email address to receive news and updates'
+//const inputM = input.map(inpu => <input type="text">{inpu}</input>)
+const button = 'Subscribe'
+
+const author = {
+    firstName: 'IYANUOLUWA',
+    lastName: 'AKINWALE'
+}
+const role = 'Software Engineer, Nigeria'
+const skills = ['HTML', 'CSS', 'JS', 'React', 'Python', 'Git', 'VSCode', 'Github']
+const skillTe = 'Skills'
+const skillsT = skills.map(skill => <p className='skills'>{skill}</p>)
+// JSX element main
+const main = (
+    <main>
+        <div className='second'>
+            {user}
+            <h3>{author.firstName} {author.lastName}</h3>
+            <p>{role}</p>
+            <h3>{skillTe}</h3>
+            <div className='skills flex'>
+                <p className="skills flex">{skillsT}</p>
+            </div>
+        </div>
+        <div className='first'>
+            <h1 className="subs">{subscribe}</h1>
+            <p className="subs">{signUp}</p>
+            <form>
+                <input type="text" placeholder="First Name" required/>
+                <input type="text" placeholder="Last Name"/>
+                <input type="email" placeholder="Email" required/><br/>
+                <button>{button}</button>
+            </form> 
+        </div>
+    </main>
+)
+
+const copyRight = 'Copyright 2020'
+
+// JSX element, footer
+const footer = (
+    <footer>
+        <div className='footer-wrapper'>
+            <p>{copyRight}</p>
+        </div>
+    </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+    <div className='app'>
+        {main}
+        {footer}
+    </div>
+)
+
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement)
--- React/tiash/src/index.js
+++ React/tiash/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-  function Square(props) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-    </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+  </button>
+  );
+}
 
 class Board extends React.Component {
   renderSquare(i) {
