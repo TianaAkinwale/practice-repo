9863451efb0fe8e235f3288d23e2547698f05376
TianaAkinwale <tianorway@gmail.com>
2022-12-24 03:29:59 +0100

    hjkl

--- JavaScript/Word Length Calculator/script.js
+++ JavaScript/Word Length Calculator/script.js
@@ -1,9 +1,46 @@
-let text = document.getElementById('text');
+let word = document.getElementById('word');
 let button = document.getElementById('button');
 let p = document.getElementById('output');
 
 button.addEventListener('click', () => {
-    abc = text.value;
-    p.innerHTML = abc.split(' ');
-    
+    abc = word.value;
+    const q = p.innerHTML
+    //use regex
+    const pattern = /\d+/g;
+    const pattern2 = ' '
+    let matches2 = q.match(pattern2)
+    let matches = q.match(pattern)
+    if (word.innerHTML = matches || matches2) {
+        errord();
+    } else {
+        p.innerHTML = abc.length;
+    }
+
+    //output when requirements are not met
+    function errord() {
+        p.innerHTML = 'only a word is allowed!'
+    }
 })
+/*const numpattern = /\d+/g;
+    const wrdpattern = ' '
+    let worderr = q.match(wrdpattern)
+    let numerr = q.match(numpattern)
+    if (word.innerHTML = numerr) {
+        errord1();
+    } else {
+        p.innerHTML = abc.length;
+    }
+    if (word.innerHTML = worderr) {
+        errord2();
+    } else {
+        p.innerHTML = abc.length;
+    }
+
+    //output when requirements are not met
+    function errord1() {
+        q = 'Only letters allowed!'
+    }
+    function errord2() {
+        q = 'Only a word allowed allowed!'
+    }
+})*
