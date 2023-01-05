704ef37e7c86c8468598e880ba60e470cf87cd54
TianaAkinwale <tianorway@gmail.com>
2023-01-05 20:48:04 +0100

    new

--- JavaScript/Word Length Calculator/script.js
+++ JavaScript/Word Length Calculator/script.js
@@ -4,43 +4,25 @@ let p = document.getElementById('output');
 
 button.addEventListener('click', () => {
     abc = word.value;
-    const q = p.innerHTML
     //use regex
-    const pattern = /\d+/g;
-    const pattern2 = ' '
-    let matches2 = q.match(pattern2)
-    let matches = q.match(pattern)
-    if (word.innerHTML = matches || matches2) {
-        errord();
-    } else {
+    const num = /\d+/g;
+    const wrd = ' ';
+    let numerr = abc.match(num)
+    let wrderr = abc.match(wrd)
+    if (word.innerHTML = numerr) {
+        numerrd()
+    } else if (word.innerHTML = wrderr) {
+        wrderrd()
+    }
+    else {
         p.innerHTML = abc.length;
     }
 
     //output when requirements are not met
-    function errord() {
+    function numerrd() {
+        p.innerHTML = 'only letters allowed!'
+    }
+    function wrderrd() {
         p.innerHTML = 'only a word is allowed!'
     }
 })
-/*const numpattern = /\d+/g;
-    const wrdpattern = ' '
-    let worderr = q.match(wrdpattern)
-    let numerr = q.match(numpattern)
-    if (word.innerHTML = numerr) {
-        errord1();
-    } else {
-        p.innerHTML = abc.length;
-    }
-    if (word.innerHTML = worderr) {
-        errord2();
-    } else {
-        p.innerHTML = abc.length;
-    }
-
-    //output when requirements are not met
-    function errord1() {
-        q = 'Only letters allowed!'
-    }
-    function errord2() {
-        q = 'Only a word allowed allowed!'
-    }
-})*
