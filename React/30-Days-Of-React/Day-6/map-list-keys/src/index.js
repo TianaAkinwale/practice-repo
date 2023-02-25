e36a84dd5c6e3c23c9d7bbedbd3aa6097b7ee1a6
TianaAkinwale <tianorway@gmail.com>
2023-02-25 06:26:21 +0100

    del

--- React/30-Days-Of-React/Day-6/map-list-keys/src/index.js
+++
@@ -1,8 +0,0 @@
-// importing the react and react-dom package and style file
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/app.js'
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
